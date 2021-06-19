fa6b3acc909d0303782f91bca52f1398f2a4c36e
EdvinAlvarado <ealvarado301@gmail.com>
2021-06-18 22:20:42 -0700

    two previous commits plus these one implemented full collision management

--- board.hpp
+++ board.hpp
@@ -11,12 +11,21 @@
 // TODO consider eliminating
 enum position {POS_FREE, POS_FILLED}; // POS_FREE = free position of the board; POS_FILLED = filled position of the board
 
+struct boolBlocked {
+	bool down;
+	bool left;
+	bool right;
+	bool rotate;
+};
+
 class Board {
 	private:
 		std::array<std::array<int,BOARD_WIDTH>,BOARD_HEIGHT> backBoard; // contains the pieces.
 	public:
 		void writeBoard(int pX, int pY, Tetromino piece);
+		void writeBackBoard(int pX, int pY, Tetromino piece);
 		bool checkLineFilled(int line);
 		std::array<std::array<int,BOARD_WIDTH>,BOARD_HEIGHT> board; // contains the pieces.
+		boolBlocked collisionChecker(int pX, int pY, Tetromino piece);
 };
 #endif
