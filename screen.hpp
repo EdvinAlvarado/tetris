85eb3a42c602ff1ebbc12df14b279a62c421cf92
EdvinAlvarado <ealvarado301@gmail.com>
2021-06-23 17:50:50 -0700

    delineated current causes of segfaults

--- screen.hpp
+++ screen.hpp
@@ -1,8 +1,8 @@
 #ifndef _SCREEN_
 #define _SCREEN_
 // TODO consider not hardcoding dimensions
-// FIXME can go from 5 to 15. cannot do #define BLOCK_SIZE		20 // Width and Height of each block of a piece
 #define BLOCK_SIZE		15 // Width and Height of each block of a piece
+// FIXME BOARD_WIDTH*BOARD_HEIGHT >= 37^2 
 #define BOARD_WIDTH		28
 #define BOARD_HEIGHT	2*BOARD_WIDTH
 #define SCREEN_WIDTH 	BOARD_WIDTH*BLOCK_SIZE
